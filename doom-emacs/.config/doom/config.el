52397ee4816c0494b9a602ca75c4efaf4972edf6
James Henderson <james@jarohen.dev>
2023-02-05 21:13:43 +0000

    set font higher on laptop

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -15,8 +15,11 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+
+(setq doom-font
+      (font-spec :size (cond
+                          ((string= (system-name) "graphite") 16)
+                          (t 12))))
 
 (setq doom-theme 'doom-tomorrow-night)
 
--- doom-emacs/.config/doom/flycheck_config.el
+++
@@ -1,1 +0,0 @@
-;;; ../../dot-emacs/doom-emacs/.config/doom/flycheck_config.el -*- lexical-binding: t; -*-
