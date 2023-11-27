0445c28c0305f02151d097618a7745f09010a363
James Henderson <james@jarohen.dev>
2023-11-27 16:59:53 +0000

    bleeding edge Clojure packages, YOLO

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -5,4 +5,8 @@
 
 (package! adoc-mode)
 
+(unpin! (:lang clojure))
 
+(package! clojure-mode :pin "525fc1b131b1fc537aa82d83d9eb2ea833cface6")
+(package! clj-refactor :pin "b476345c580ae7cbc6b356ba0157db782684c47f")
+(package! cider :pin "d16988719ae5561f32b06db0726adb03d84fe15c")
