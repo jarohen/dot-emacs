b4572447b7e085506a80c32a092d25d79a2efa2e
James Henderson <james@jarohen.dev>
2023-11-27 16:59:44 +0000

    unpin packages after emacs 29 release

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -5,12 +5,4 @@
 
 (package! adoc-mode)
 
-;; pin, remove after emacs 29 release
-(package! transient
-      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
-      :recipe (:host github :repo "magit/transient"))
 
-;; pin, remove after emacs 29 release
-(package! with-editor
-          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
-          :recipe (:host github :repo "magit/with-editor")) 
