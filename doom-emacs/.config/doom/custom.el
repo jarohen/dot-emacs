255204203d405d09bd5f6246291797ec65b6fa4f
James Henderson <james@jarohen.dev>
2024-04-15 09:50:27 +0100

    lost some customisation, apparently

--- doom-emacs/.config/doom/custom.el
+++ doom-emacs/.config/doom/custom.el
@@ -8,12 +8,22 @@
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
    '("~/mnt/jms/journal.org" "~/mnt/jms/team-journal" "~/src/xtdb/xtdb-team" "~/src/xtdb/xtdb-leads"))
+ '(package-selected-packages '(tree-sitter-langs astro-ts-mode elcord ebnf-mode))
  '(safe-local-variable-values
-   '((cider-ns-refresh-after-fn . "integrant.repl/resume")
+   '((cider-repl-display-help-banner)
+     (cider-clojure-cli-aliases ":xtdb")
+     (cider-clojure-cli-aliases . ":xtdb:dev")
+     (cider-preferred-build-tool . gradle)
+     (cider-clojure-cli-aliases . ":core2:dev")
+     (eval add-to-list 'cider-test-defining-forms "def-slt-test")
+     (eval define-clojure-indent
+      (match 1)
+      (for-all 1))
+     (cider-ns-refresh-after-fn . "integrant.repl/resume")
      (cider-ns-refresh-before-fn . "integrant.repl/suspend"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
