5cdc8b54315f106ad0e8d34bbbfb93fe8a54c76b
James Henderson <james@jarohen.dev>
2025-01-11 19:49:08 +0000

    bump doom

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -4,10 +4,6 @@
 (package! evil-cleverparens)
 (package! adoc-mode)
 
-(package! clojure-mode :pin "525fc1b131b1fc537aa82d83d9eb2ea833cface6")
-(package! clj-refactor :pin "b476345c580ae7cbc6b356ba0157db782684c47f")
-(package! cider :pin "d16988719ae5561f32b06db0726adb03d84fe15c")
-
 (package! keycast)
 (package! astro-ts-mode)
 
