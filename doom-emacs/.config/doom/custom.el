8d4c0d51acfaeb23ec8239d23c3e956c2d97a5fb
James Henderson <james@jarohen.dev>
2025-03-11 21:15:31 +0000

    bump doom

--- doom-emacs/.config/doom/custom.el
+++ doom-emacs/.config/doom/custom.el
@@ -31,4 +31,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(line-number-current-line ((t (:inherit (hl-line default) :background "dim gray" :foreground "#839496" :weight normal))))
+ '(org-block-end-line ((t (:inherit org-block-begin-line))))
  '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
