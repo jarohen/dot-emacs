0d8bb1986e1f2c86ecd4db1969a2c06ce77ffc4d
James Henderson <james@jarohen.dev>
2025-04-17 10:31:39 +0100

    try to get submodule to track master, bump some modules

--- doom-emacs/.config/doom/packages.el
+++ doom-emacs/.config/doom/packages.el
@@ -7,10 +7,6 @@
 (package! keycast)
 (package! astro-ts-mode)
 
-(package! copilot
-  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+(package! copilot)
 
 (package! ebnf-mode)
-
-(package! smartparens
-  :recipe (:host github :repo "Fuco1/smartparens" :fork "tomdl89/smartparens" :branch "fix-sp-region-ok-p-regex"))
